c1b101447432308212b5a9196066835437dfcc9b
black-desk <me@black-desk.cn>
2023-10-16 17:56:08 +0800

    fix: allow '-' in scope

--- conventionalcommits-checker/parser_test.go
+++ conventionalcommits-checker/parser_test.go
@@ -100,6 +100,18 @@ func TestParseSubjectLine(t *testing.T) {
 				Valid: false,
 			},
 		},
+		{
+			name: "commit message with complex scope",
+			args: args{
+				subject: "chore(tools/some-tools): xxx",
+			},
+			want: SubjectData{
+				Valid:       true,
+				Type:        "chore",
+				Scope:       "tools/some-tools",
+				Description: "xxx",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
